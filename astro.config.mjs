c5296f2b61a8cb9e5dfe76e5724cef5dd3029973
npmstudy <npmstudy@qq.com>
2024-05-21 23:37:09 +0800

    document, please ignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [starlight({
+    title: "My delightful docs site"
+  })]
+});
